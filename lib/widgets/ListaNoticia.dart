d752dd6bc08ba3d8402877d657d0d965f9f6aca0
CodeBreaker <apps@codebreaker.com.br>
2019-04-18 10:27:47 -0300

    Criação da class Noticia
    - Ajustes ListaNoticia

--- lib/widgets/ListaNoticia.dart
+++ lib/widgets/ListaNoticia.dart
@@ -1,3 +1,4 @@
+import 'package:exemplo_flutternews/api/NewsApi.dart';
 import 'package:flutter/material.dart';
 
 class ListaNoticia extends StatefulWidget{
@@ -15,11 +16,50 @@ final state = _NoticeListPageState();
 
 class _NoticeListPageState extends State<ListaNoticia>{
 
+  List _news = new List();
+  var repositorio = new NewsApi();
+
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new Container();
+    return new Scaffold(
+
+      appBar: AppBar(),
+
+      body: new Container(
+        child: _getListViewWidget(),
+      ),
+
+    );
   }
 
+
+  Widget _getListViewWidget(){
+
+    var list = new ListView.builder(
+
+      itemCount: _news.length,
+      padding: new EdgeInsets.only(top: 5.0),
+
+      itemBuilder: (context, index){
+        return _news[index];
+      },
+
+    );
+
+    return list;
+  }
+
+
+
+
+
+
+
+
+
+
+
 }
+
