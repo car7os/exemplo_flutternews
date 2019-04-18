44c4b14d24c6622a3ef578e2205b01f666cb26c2
CodeBreaker <apps@codebreaker.com.br>
2019-04-18 00:04:29 -0300

    Criação da class Noticia
    - Criando Widget do Item da Lista

---
+++ lib/widgets/Noticia.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/widgets.dart';
+
+class Noticia extends StatelessWidget{
+
+  var _img;
+  var _titulo;
+  var _data;
+  var _descricao;
+
+  Noticia(this._img, this._titulo, this._data, this._descricao);
+
+
+  @override
+  Widget build(BuildContext context) {
+
+      BuildContext _context;
+
+    // TODO: implement build
+    return new Container();
+  }
+
+}
