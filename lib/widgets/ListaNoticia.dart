e68205c632b3912ddf0a4e9e67acdfda06a941bd
CodeBreaker <apps@codebreaker.com.br>
2019-04-18 09:03:17 -0300

    Criação da class Noticia
    - Criando a lista de notícias

---
+++ lib/widgets/ListaNoticia.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ListaNoticia extends StatefulWidget{
+
+final state = _NoticeListPageState();
+
+
+@override
+  _NoticeListPageState createState() => state;
+
+
+
+}
+
+
+class _NoticeListPageState extends State<ListaNoticia>{
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container();
+  }
+
+}
